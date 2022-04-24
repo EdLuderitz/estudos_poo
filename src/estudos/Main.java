8114053e2b4a0972b617ce3d3860fa511bb3ea28
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-04-24 01:51:11 -0300

    Criada a Classe Aluno

---
+++ src/estudos/Main.java
@@ -0,0 +1,8 @@
+package estudos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/estudospoo/classes/Aluno.java
@@ -0,0 +1,17 @@
+package estudospoo.classes;
+
+/* Está é nossa classe/objeto que representa o aluno */
+public class Aluno {
+
+    String nome;
+    int idade;
+    String dataNascimento;
+    String registroGeral;
+    String numeroCpf;
+    String nomeMae;
+    String nomePai;
+    String dataMatricula;
+    String nomeEscola;
+    String serieMatriculado;
+
+}
