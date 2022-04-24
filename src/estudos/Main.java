599d119e66b71cefeea4439fef5598215e41bfdb
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-04-24 02:49:50 -0300

    Definindo os construtores dos objetos

--- src/estudos/Main.java
+++ src/estudos/Main.java
@@ -1,8 +1,20 @@
 package estudos;
 
+import estudospoo.classes.Aluno;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Aluno aluno1 = new Aluno(); /* Este será o João */
+
+        Aluno aluno2 = new Aluno(); /* Este será o Pedro */
+
+        Aluno aluno3 = new Aluno(); /* Este será o Alex */
+
+        Aluno aluno4 = new Aluno("Maria");
+
+        Aluno aluno5 = new Aluno("José", 50);
 
     }
 }
