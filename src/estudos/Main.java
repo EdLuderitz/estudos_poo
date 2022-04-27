52c762735010af401eeeb6947b680b03ed0833c2
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-04-26 23:57:25 -0300

    Retirado comentários desnecessários do código

--- src/estudos/Main.java
+++ src/estudos/Main.java
@@ -8,15 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* Aluno é um atributo */
-        /* new Aluno() é uma instância (Criação de objeto) */
-        /* a variavel aluno1 é uma referência para o objeto aluno*/
-
         String nome = JOptionPane.showInputDialog("Qual o nome do Aluno(a)?");
 
         Aluno aluno1 = new Aluno();
         aluno1.setNome(nome);
-        /*
+
         String idade = JOptionPane.showInputDialog("Qual a idade de " + aluno1.getNome() + "?");
         String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento de " + aluno1.getNome() + "?");
         String registroGeral = JOptionPane.showInputDialog("Qual o registro geral de " + aluno1.getNome() + "?");
@@ -26,7 +22,6 @@ public class Main {
         String dataMatriculada = JOptionPane.showInputDialog("Qual a data da matricula de " + aluno1.getNome() + "?");
         String serieMatriculada = JOptionPane.showInputDialog("Qual qual a serie matriculada de " + aluno1.getNome() + "?");
         String nomeEscola = JOptionPane.showInputDialog("Qual o nome da escola de " + aluno1.getNome() + "?");
-         */
         String disciplina1 = JOptionPane.showInputDialog("Qual o nome da disciplina 1 de " + aluno1.getNome() + "?");
         String nota1 = JOptionPane.showInputDialog("Qual a nota 1 de " + aluno1.getNome() + "?");
         String disciplina2 = JOptionPane.showInputDialog("Qual o nome da disciplina 2 de " + aluno1.getNome() + "?");
@@ -35,7 +30,7 @@ public class Main {
         String nota3 = JOptionPane.showInputDialog("Qual a nota 3 de " + aluno1.getNome() + "?");
         String disciplina4 = JOptionPane.showInputDialog("Qual o nome da disciplina 4 de " + aluno1.getNome() + "?");
         String nota4 = JOptionPane.showInputDialog("Qual a nota 4 de " + aluno1.getNome() + "?");
-/*
+
         aluno1.setIdade(Integer.valueOf(idade));
         aluno1.setDataNascimento(dataNascimento);
         aluno1.setRegistroGeral(registroGeral);
@@ -45,7 +40,6 @@ public class Main {
         aluno1.setDataMatricula(dataMatriculada);
         aluno1.setSerieMatriculado(serieMatriculada);
         aluno1.setNomeEscola(nomeEscola);
- */
         aluno1.setDisciplina1(disciplina1);
         aluno1.setDisciplina2(disciplina2);
         aluno1.setDisciplina3(disciplina3);
